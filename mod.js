3346e8ffa0a6be9764ab187dece801842d0eb010
levlups <52762684+levlups@users.noreply.github.com>
2019-12-25 07:04:01 -0500

    mod js file
    main  javascript file to mod Galactica Unbound

---
+++ mod.js
@@ -0,0 +1,51 @@
+repeat(3,'john');
+
+wait(10,'wait is working');
+var metoo=bop()
+function bop(){
+	console.log('bop');
+	
+}
+
+wait(15,metoo);
+
+
+var playerMovementState = noa.entities.getMovement(noa.playerEntity);
+
+/*playerMovementState.airJumps = 0;
+playerMovementState.maxSpeed = 8.5;
+playerMovementState.jumpImpulse = 7;
+playerMovementState.jumpTime = 150;*/
+
+var datmesh = noa.entities.getMeshData(noa.playerEntity)
+//datmesh.mesh.setEnabled(false);
+//console.log(voxelCrunch);
+//blocks.sand.texture.src="textures/stone.png"
+/*console.log(tools)
+//itemBarItems[0]="item_sword";
+var datmesh = noa.entities.getMeshData(noa.playerEntity)
+						datmesh.mesh._children[0].scaling.y=1.5
+						datmesh.mesh._children[0].scaling.x=1.5
+						datmesh.mesh._children[0].scaling.z=1.5
+						
+						var texture1 = new BABYLON.Texture("textures/player/newp.png", scene, false, true, BABYLON.Texture.NEAREST_SAMPLINGMODE);
+						
+						datmesh.mesh._children[0].material.diffuseTexture=texture1
+						
+console.log(seed);
+console.log(noa.registry.blocks)
+setInterval(function() {
+	console.log('cool MOD');
+	
+						}, 1000);
+						
+						
+						setInterval(function() {
+								noa.setBlock(2, [4,6,4]);
+									noa.setBlock(2, [4,7,4]);
+										noa.setBlock(2, [4,8,4]);
+										var playerbody=noa.entities.getPhysicsBody(noa.playerEntity)
+	  playerbody.applyImpulse([0,10, 4])
+	 // console.log(noa);
+	
+						}, 10000*/
